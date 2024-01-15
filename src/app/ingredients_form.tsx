23426c3a3ecd0573ff4fcff03841e7f6f93b9fe0
Nile Kelly <nile@NKs-MacBook-Pro.local>
2024-01-15 15:31:30 +0000

    wip ingredients form + basic styling

---
+++ src/app/ingredients_form.tsx
@@ -0,0 +1,81 @@
+'use client'
+import React, { useState } from "react";
+
+interface IngredientField {
+    quantity: string;
+    ingredient: string;
+}
+
+export default function IngredientsForm() {
+    const [ingredients, setIngredients] = useState<IngredientField[]>([
+        { quantity: "", ingredient: "" },
+    ]);
+
+    const handleAddField = () => {
+        setIngredients([...ingredients, { quantity: "", ingredient: "" }]);
+    };
+
+    // const handleRemoveField = (index: number) => {
+    //     const newIngredients = ingredients.filter((_, i) => i !== index);
+    //     setIngredients(newIngredients);
+    // };
+
+    const handleChange = (
+        index: number,
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        const newIngredients = ingredients.map((ingredient, i) => {
+            if (i === index) {
+                return { ...ingredient, [event.target.name]: event.target.value };
+            }
+            return ingredient;
+        });
+
+        setIngredients(newIngredients);
+    };
+
+    const handleSubmit = (
+    ) => {
+        console.log('Form submitted.')
+        ingredients.forEach(ingredient => {
+            console.log(`Ingredient: ${ingredient.ingredient}, Quantity: ${ingredient.quantity}`)
+        })
+    }
+
+    return (
+        <form>
+            {ingredients.map((ingredient, index) => (
+                <div key={index}>
+                    <div>
+                        <input
+                            className="input-field"
+                            name="ingredient"
+                            value={ingredient.ingredient}
+                            onChange={(event) => handleChange(index, event)}
+                            placeholder="Ingredient"
+                        />
+                        <input
+                            className="input-field"
+                            name="quantity"
+                            value={ingredient.quantity}
+                            onChange={(event) => handleChange(index, event)}
+                            placeholder="Quantity"
+                        />
+                    </div>
+                </div>
+            ))}
+            <div>
+                <button type="button" onClick={handleAddField} className="add-button">
+                    +
+                </button>
+            </div>
+            <div>
+                <button type="button" onClick={handleSubmit} className="submit-button">
+                    Submit
+                </button>
+            </div>
+        </form>
+
+    );
+}
+
