991faa2e9ae930391ac27c4f750d5ba407b3ad75
Nile Kelly <nile@NKs-MacBook-Pro.local>
2024-01-15 15:14:47 +0000

    removed boilerplate from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
-import Image from 'next/image'
+import IngredientsForm from "./ingredients_form";
 
 export default function Home() {
   return (
-    <main>
+    <main className="main">
+      <h1 className="title"> RecipeGPT</h1>
+      <h2 className="subtitle">Create delicious recipes from any ingredients using artificial intelligence</h2>
+      <h3 className="instruction"> Enter your ingredients below and press submit to generate a mouth-watering recipe.</h3>
+      <section className="form-section">
+        <IngredientsForm />
+      </section>
     </main>
   )
 }
